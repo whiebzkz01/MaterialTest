50b2079f2b115e7954c98fe89da62200d127c656
whiebzkz <kongzhenzhu@foxmail.com>
2017-03-14 19:19:58 +0800

    Fisrt commit

---
+++ app/src/main/java/com/whieb/materialtest/Fruit.java
@@ -0,0 +1,31 @@
+package com.whieb.materialtest;
+
+/**
+ * Created by zhukongzhen on 2017/3/14.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
